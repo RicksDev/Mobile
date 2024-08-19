ee8559220a04e390c1b673b982a34103670e3340
RicksDev <filho.ricardo13@gmail.com>
2024-08-19 13:45:21 -0300

    Finalizando projeto

--- PrimeiraAulaGio/js/script.js
+++ PrimeiraAulaGio/js/script.js
@@ -1,5 +1,6 @@
 console.log('Testando');
 
+//Estou com problema para criar o botão de excuir em uma só linha.
 
 const button = document.querySelector('.form-button');
 const nome = document.querySelector("#nome");
@@ -30,6 +31,28 @@ button.addEventListener("click", (e) => {
     // Incluindo itens no HTML
     // O + vai incrementando os valores
      lista.innerHTML += templateHTML;
+
+     const li = document.createElement("li");
+    li.textContent = `${inputName} / ${inputIdade} / ${inputTelefone}`;
+
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Excluir";
+    deleteButton.style.marginLeft = "100px"; 
+    deleteButton.classList.add("botao");
+
+    
+    deleteButton.addEventListener("click", () => {
+        lista.removeChild(li);
+    });
+
+    
+    li.appendChild(deleteButton);
+
+    
+    lista.appendChild(li);
+
+
 
     //  Limpar o campo
      nome.value = "";
