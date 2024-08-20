383ee13dc911f17c3799fc8efcf5592fea726d3c
RicksDev <filho.ricardo13@gmail.com>
2024-08-20 16:35:38 -0300

    Subindo começo de projeto do progessor Samuel

---
+++ minha_tela_basica/lib/view/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(title: Text('Tela do Ricks'),),
+    body: Column(
+      children: [
+        
+      ],
+    ),
+
+    );
+  }
+}
