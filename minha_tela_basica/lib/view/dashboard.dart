1c2a8dd4bb38cbb606b00f025cfaef1c7d78e539
RicksDev <filho.ricardo13@gmail.com>
2024-08-21 16:10:33 -0300

    Subindo projeto desafio de Flutter

--- minha_tela_basica/lib/view/dashboard.dart
+++ minha_tela_basica/lib/view/dashboard.dart
@@ -1,18 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:minha_tela_basica/widgets/bannerCard.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key, required String title});
 
+  get decoration => null;
+
+  get inputBorder => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(title: Text('Tela do Ricks'),),
-    body: Column(
-      children: [
-        
-      ],
-    ),
-
+      appBar: AppBar(
+        title: Text('Tela do Ricks'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+                height: 100,
+                width: 500,
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: inputBorder,
+                    border: inputBorder,
+                    focusedBorder: inputBorder,
+                    hintText: 'Pesquisar na loja',
+                    prefixIcon: const Icon(Icons.search),
+                    fillColor: const Color.fromARGB(
+                        255, 237, 234, 234), // Para preencher precisa ativar!
+                    filled: true,
+                  ),
+                )),
+            bannerCard(),
+            SizedBox(
+              height: 20,
+            ),
+            bannerCard(),
+            SizedBox(
+              height: 20,
+            ),
+            bannerCard(),
+            SizedBox(
+              height: 20,
+            ),
+            bannerCard(),
+            SizedBox(
+              height: 100,
+            ),
+            Container(
+                height: 60,
+                width: 600,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.green,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      
+                      Text(
+                        'Go to Checkout',
+                        style: TextStyle(fontSize: 19),
+                      ),
+                      Spacer(),
+                      Text('TOTAL = \$***')
+                    ],
+                  ),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ minha_tela_basica/lib/widgets/bannerCard.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class bannerCard extends StatelessWidget {
+  const bannerCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      width: 700,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: const Color.fromARGB(255, 210, 201, 200),
+          border: Border.all(
+            color: Colors.black,
+          )),
+      child: Row(
+        children: [
+          Image.asset("assets/images/Abacaxi.png"),
+          SizedBox(
+            width: 160,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Banana Prata',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                '1 Kg de banana',
+                style: TextStyle(fontSize: 12),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.remove_circle_outline),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Text('1'),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Icon(Icons.add_circle_outline),
+                ],
+              )
+            ],
+          ),
+          Spacer(),
+          Container(
+              height: 120,
+              width: 100,
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 2,
+                      right: 23,
+                      child: IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {},
+                      )),
+                  Positioned(
+                      bottom: 8,
+                      right: 19,
+                      child: Text(
+                        '\$4.99',
+                        style: TextStyle(fontSize: 18),
+                      ))
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
